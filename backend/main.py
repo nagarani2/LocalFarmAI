9decc8c2c3255cd7bdd1dd2745e4bb7c05737b0b
Nagarani Kondapuram <nagaranikondapuram@gmail.com>
2026-09-19 22:22:32 +0530

    Allow deployed frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "http://localfarmai.onrender.com"
+        "https://localfarmai.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
